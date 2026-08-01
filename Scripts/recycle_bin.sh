548d1d5e2de1848e5b9161c6601c146ff82ee01d
Maximilien <maximilienmaison@orange.fr>
2026-08-01 19:54:30 +0200

    trash-rework: starting over the recycling_bin

--- Scripts/recycle_bin.sh
+++ Scripts/recycle_bin.sh
@@ -1,267 +1,70 @@
-#!/bin/sh
+#/bin/sh
 
-### --- USER PARAMETERS ---
+### === EDITABLE CONFIG ===
 
-bin_pathname=/home/virtual.maximilien/Desktop/recycle_bin
+DIR="$HOME"
+BIN="$DIR/.trash"
 
-### --- Global Variables ---
-mode=""
-files=""
+### === PROGRAM ===
 
-### --- Functions ---
-create_bin(){
-    if [ ! -e "$bin_pathname" ]; then
-        mkdir "$bin_pathname"
-        echo "Created bin"
+# === Global Vars ===
+
+TABLE="$BIN/IAmTheTable"
+NONE="/dev/null"
+
+help () {
+    echo "This script allows one to have a recycle bin rather than definitive deletation aka rm."
+    echo ""
+    echo "Usage: \`$0' [delete|info|restore|empty] <files?>"
+    echo " - delete,  moves the specified files to the bin."
+    echo "            Fail if no files are provided or mv issues"
+    echo " - info,    list bin's location and the files contained in the bin."
+    echo " - restore, moves back the files from where they were deleted."
+    echo "            Fail if no files are provided or mv issues"
+    echo " - empty,   removes all files contained in the bin."
+}
+
+isBinAlive () {
+    if [ -e "$BIN" ] && [ -d "$BIN" ] && [ -f "$TABLE" ]; then
+        echo 0
     else
-        echo "bin path is : $bin_pathname"
+        echo 1
     fi
 }
 
-abspath() {
-    path=$1
-    case "$path" in
-        /*) ;;
-        *)  path=$(pwd -P)/$path ;;
-    esac
-
-    saved_IFS=$IFS
-    IFS=/
-    set -- $path
-    IFS=$saved_IFS
-    out=
-    for part do
-        case "$part" in
-            ""|.)
-                continue
-                ;;
-            ..)
-                out=${out%/*}
-                ;;
-            *)
-                out=$out/$part
-                ;;
-        esac
-    done
-    out="${out:-/}"
-    echo "${out%/*}"
-}
-
-strip_path(){
-    echo "$*" | sed 's,/$,,' | sed "s,^.*/,,"
-}
-
-extract_index(){
-    var=$(echo "$1" | grep -o "[0-9]*$")
-    if [ -z "$var" ];then
-        var=0
+createBin () {
+    if [ $(isBinAlive) -eq 0 ]; then
+        echo "Bin already exist at $BIN"
+    else
+        mkdir -- "$BIN"
+        touch -- "$TABLE"
     fi
-    echo $var
 }
 
-delete_with_conflict(){
-    name="$1"
-    full_path="$2"
-    main_log="$bin_pathname/.log_$name"
-
-    log_content=""
-    if [ -e "$main_log" ]; then 
-        IFS= read -r log_content < "$main_log"
+empty () {
+    if [ $(isBinAlive) -eq 0 ]; then
+        rm -rf "$BIN"/* "$BIN"/.*
+        touch -- "$TABLE"
+    else
+        echo "Bin does not exist!"
     fi
+}
 
-    if [ "conflict" = "$log_content" ];then
-        max=0
-        for filename in "$bin_pathname/$name/"*;do
-            if [ -e "$filename" ]; then
-                base=$(strip_path "$filename")
-                idx=$(extract_index "$base")
-                if [ "$idx" -gt "$max" ]; then
-                    max=$idx
-                fi
+delete () {
+    for file in $@; do
+        if [ -e $file ]; then
+            $(mv $file $BIN > $NONE 2>&1) && if [ -d $file ]
+            then
+                # rename as foldX
+                # write data as fold to table
+            else
+                # rename as fileX
+                # write data as file to table
             fi
-        done
-        next=$(($max+1))
-        mv "$full_path/$name" "$bin_pathname/$name/${name}__$next"
-        echo  "$full_path/$name" > "$bin_pathname/$name/.log_${name}__$next"
-
-    else #Resolving a conflict for the first time
-        mv "$bin_pathname/$name" "$bin_pathname/${name}__0"
-        mkdir "$bin_pathname/$name"
-        mv "$bin_pathname/${name}__0" "$bin_pathname/$name"
-
-        if [ -e "$main_log" ]; then
-            mv "$main_log" "$bin_pathname/$name/.log_${name}__0"
+        else
+            echo "User tried to delete a none existing file at $file"
         fi
-
-        mv "$full_path/$name" "$bin_pathname/$name/${name}__1"
-        echo "$full_path/$name" > "$bin_pathname/$name/.log_${name}__1"
-
-        echo "conflict" > "$bin_pathname/.log_$name"
-    fi
-
-        echo "moved $name to the bin"
-}
-
-delete(){
-    while [ $# -ne 0 ]; do
-        full_path=$(abspath "$1")
-        if [ ! -e $full_path ]; then
-            echo "$1 was not been found"
-            shift
-            continue
-        fi
-
-        name=$(strip_path "$1")
-
-        if [ -e "$bin_pathname/$name" ];then
-            delete_with_conflict "$name" "$full_path"
-            shift
-            continue
-        fi
-
-        mv "$full_path/$name" "$bin_pathname"
-        echo  "$full_path/$name" > "$bin_pathname/.log_$name"
-        echo "moved $1 to the bin"
-        shift
     done
 }
 
-empty(){
-    if [ ! -e "$bin_pathname" ]; then
-        echo "bin doesn't exist"
-        exit
-    fi
-    rm -rf "$bin_pathname"/*
-    rm -rf "$bin_pathname"/.log_*
-    echo "The bin has been emptied"
-}
-
-restore_with_conflicts(){
-    conflict_fold=$2
-    name=$(strip_path "$1")
-    log_path="$bin_pathname/$conflict_fold/.log_$name"
-    if [ ! -e $log_path ]; then
-        echo "$1 is an untracked file"
-        return;
-    fi
-
-    log_content=""
-    if [ -e "$log_path" ]; then
-        IFS= read -r log_content < "$log_path"
-    fi
-
-    echo "restoring $name to $log_content"
-    mv "$1" "$log_content"
-    rm $log_path
-}
-
-restore(){
-    while [ $# -ne 0 ]; do
-        log_path="$bin_pathname/.log_$1"
-        if [ ! -e $log_path ]; then
-            echo "$1 is an untracked file"
-            shift
-            continue
-        fi
-
-        log_content=""
-        if [ -e "$log_path" ]; then
-            IFS= read -r log_content < "$log_path"
-        fi
-
-        if [ "conflict" = "$log_content" ];then
-            conflict_fold=$1
-            for file in "$bin_pathname/$1/"*;do
-                restore_with_conflicts "$file" "$conflict_fold"
-            done
-            rmdir "$bin_pathname/$conflict_fold"
-            rm "$bin_pathname/.log_$conflict_fold"
-            shift
-            continue
-        fi
-
-        echo "restoring $1 to $log_content"
-        mv "$bin_pathname/$1" "$log_content"
-        rm $log_path
-        shift
-    done
-}
-
-print_help(){
-    echo "Here is some help"
-}
-
-parse_cmd(){
-    while [ $# -ne 0 ]; do
-        case "$1" in
-            delete)
-                mode="delete"
-                shift
-                ;;
-            empty)
-                mode="empty"
-                shift
-                ;;
-            restore)
-                mode="restore"
-                shift
-                ;;
-            -h|--help)
-                print_help
-                exit
-                ;;
-            -p|--path)
-                echo "Bin is located to : $bin_pathname"
-                exit
-                ;;
-            -i|--info)
-                ls $bin_pathname
-                exit
-                ;;
-            -i+|--info+)
-                ls -a $bin_pathname
-                exit
-                ;;
-            -i++|--info++)
-                tree $bin_pathname -a
-                exit
-                ;;
-            *)
-                break
-                ;;
-        esac
-    done
-    files=$@
-}
-
-### --- Main Script ---
-
-bin_pathname=$(echo $bin_pathname | sed 's,/$,,')
-
-parse_cmd $@
-
-if [ -z "$files" ] && [ "$mode" = "delete" ]; then 
-    echo "Please provide files or folders to delete" 
-    exit
-fi
-if [ -z "$files" ] && [ "$mode" = "restore" ]; then 
-    echo "Please provide files or folders to restore" 
-    exit
-fi
-
-create_bin
-
-if [ "$mode" = "delete" ]; then
-    delete $files
-    exit
-fi
-
-if [ "$mode" = "empty" ]; then
-    empty
-    exit
-fi
-
-if [ "$mode" = "restore" ]; then
-    restore $files
-    exit
-fi
+# if $DIR doesnt exist
